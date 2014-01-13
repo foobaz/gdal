7af33bf7dd978332146eb9c2e236e0501eb4932d
William MacKay <foobaz@gmail.com>
2014-01-13 12:40:26 -0800

    replace pkg-config with manual CFLAGS & LDFLAGS for compatibility with Debian 7

--- osr.go
+++ osr.go
@@ -4,7 +4,8 @@ package gdal
 #include "go_gdal.h"
 #include "gdal_version.h"
 
-#cgo linux  pkg-config: gdal
+#cgo linux  CFLAGS: -I/usr/include/gdal
+#cgo linux  LDFLAGS: -lgdal
 #cgo darwin pkg-config: gdal
 #cgo windows LDFLAGS: -lgdal.dll
 */
